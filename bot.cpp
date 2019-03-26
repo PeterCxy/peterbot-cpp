c5dd38a9c8c4001f72a083594add93c6b8ab1444
Peter Cai <peter@typeblog.net>
2019-03-26 08:25:20 +0800

    bot: split loop() into two functions for better look

--- bot.cpp
+++ bot.cpp
@@ -24,27 +24,31 @@ void PeterBot::run() {
 void PeterBot::loop(unsigned long offset) {
     this->mClient.getUpdates(offset, 60 * 5,
         [this, offset](TelegramClient *client, json *res, int code) {
-            if (code != 200) {
-                std::cerr << "Something happened during long-polling: " << code << std::endl;
-                std::cerr << "Continuing anyway..." << std::endl;
-                this->loop(offset);
-                return;
-            }
-            
-            unsigned long new_offset = offset;
-            for (auto& upd : *res) {
-                if (upd["update_id"].get<unsigned long>() > new_offset) {
-                    new_offset = upd["update_id"];
-                }
-
-                if (upd.contains("message")) {
-                    // This is a message
-                    json msg = upd["message"].get<json>();
-                    if (msg.contains("text"))
-                        std::cout << msg["text"].get<std::string>() << std::endl;
-                }
-            }
-
-            this->loop(new_offset + 1);
+            this->onUpdates(offset, res, code);
         });
+}
+
+void PeterBot::onUpdates(unsigned long offset, json *res, int code) {
+    if (code != 200) {
+        std::cerr << "Something happened during long-polling: " << code << std::endl;
+        std::cerr << "Continuing anyway..." << std::endl;
+        this->loop(offset);
+        return;
+    }
+            
+    unsigned long new_offset = offset;
+    for (auto& upd : *res) {
+        if (upd["update_id"].get<unsigned long>() > new_offset) {
+            new_offset = upd["update_id"];
+        }
+
+        if (upd.contains("message")) {
+            // This is a message
+            json msg = upd["message"].get<json>();
+            if (msg.contains("text"))
+                std::cout << msg["text"].get<std::string>() << std::endl;
+        }
+    }
+
+    this->loop(new_offset + 1);
 }
