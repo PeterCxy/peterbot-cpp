7db1a72b9230175ef6d22e17b62b77d19d531902
Peter Cai <peter@typeblog.net>
2019-03-28 18:40:26 +0800

    refactor: generate Telegram data types using macros

--- tg_types.hpp
+++ tg_types.hpp
@@ -1,14 +1,67 @@
 #ifndef TG_TYPES_HPP
 #define TG_TYPES_HPP
+#include "util.hpp"
 #include <nlohmann/json.hpp>
 #include <optional>
 #include <type_traits>
 #include <vector>
 
+// A macro to simply concat two lines of code
+// difference from `CAT`: this does NOT concat tokens
+// just the lines of code.
+#define MY_CAT(x, y) x; y
+// The folding function used when generating the struct fields
+// converts `(type) name` to `type name;` using `PAIR` macro
+// which I took from somewhere else and folds them together
+// Basically, the `MY_CAT` is recursively applied onto the result
+// of the previous application (deferred, not expanded yet)
+// and the new argument, the result is kind of like
+// > MY_CAT(MY_CAT(MY_CAT(arg1, PAIR(arg2)), PAIR(arg3)), PAIR(arg4)), PAIR(arg5)
+// Note how the `__VA_ARGS__` is used here to enable recursive
+// application. Also note that,
+// 1. the final result lacks a final layer of `MY_CAT`. This can
+//    be simply fixed by calling the most outer macro with an
+//    extra empty argument, which is exactly what we do later in
+//    this file.
+// 2. the first argumeng `arg1` needs to be pre-processed because
+//    it cannot be processed in this macro (we recursively apply
+//    the same thing over and over again).
+#define TELEGRAM_TYPE_GENERATOR_STRUCT(x, y, ...) \
+    MY_CAT(x, PAIR(y)), __VA_ARGS__
+// The folding function used to generate the `from_json` function
+// it generates each assignment from json to the actual fields.
+#define TELEGRAM_TYPE_GENERATOR_FROM_JSON(x, y, ...) \
+    MY_CAT(x, _TELEGRAM_TYPE_GENERATOR_FROM_JSON(STRIP(y), TYPEOF(y))), __VA_ARGS__
+// Inner macro to be called by TELEGRAM_TYPE_GENERATOR_FROM_JSON
+// `x` should be the name of the field while `t` should be the type
+// of the field. This double-layer is needed because the `STR()` macro
+// will cancel the effect of `STRIP`, essentially converting `STRIP(x)`
+// itself into a string literal, which is not what we want.
+#define _TELEGRAM_TYPE_GENERATOR_FROM_JSON(x, t) \
+    ret.x = get_from_json<t>(j, STR(x))
+
+// A macro to generate a struct with `from_json` method
+// corresponding to a Telegram API message type.
+// See below inside Telegram namespace for usage.
+// Note that struct fields should be defined with `(type) name`
+// so that this macro can properly process them by
+// generating the fields and also the `from_json` function.
+#define TELEGRAM_TYPE_GENERATE(name, x, ...) \
+    struct name { \
+        M(EVAL(WHILE(PRED, TELEGRAM_TYPE_GENERATOR_STRUCT, PAIR(x), __VA_ARGS__))); \
+        static name from_json(json *j) { \
+            name ret; \
+            M(EVAL(WHILE(PRED, TELEGRAM_TYPE_GENERATOR_FROM_JSON, \
+                _TELEGRAM_TYPE_GENERATOR_FROM_JSON(STRIP(x), TYPEOF(x)), __VA_ARGS__))); \
+            return ret; \
+        } \
+    };
+
 namespace Telegram {
     using namespace nlohmann;
 
     // Source: <https://gist.github.com/fenbf/d2cd670704b82e2ce7fd>
+    // `value` is true when T has `from_json`
     template<typename T>
     class Deserializable {
         private:
@@ -25,61 +78,76 @@ namespace Telegram {
             };
     };
 
+    // A type_trait helper to determine whether a template argument U
+    // is in the form of `std::optional<T>`. If so, `T` is also unwrapped
+    // inside the helper as `inner_type` for easier use.
+    template<typename>
+    struct IsOptional : std::false_type {};
     template<typename T>
-    typename std::enable_if<Deserializable<T>::notValue, T>::type
+    struct IsOptional<std::optional<T>> : std::true_type {
+        typedef T inner_type;
+    };
+
+    // General case: T is some normal type which is not a `Deserializable`
+    // nor an optional type. In this case, we rely on `nlohmann_json` to
+    // properly parse the result for us. This is expected to work only
+    // for primitive types and not any other complicated types.
+    template<typename T>
+    typename std::enable_if<
+        Deserializable<T>::notValue && std::negation<IsOptional<T>>::value, T>::type
     get_from_json(json *j, const char* key) {
         return (*j)[key].get<T>();
     }
 
+    // If T is itself an Telegram data type with my custom `from_json` method,
+    // deserialize it from its own `from_json` method, which may in turn
+    // call the general case of `get_from_json` again.
     template<typename T>
     typename std::enable_if<Deserializable<T>::value, T>::type
     get_from_json(json *j, const char* key) {
         return T::from_json(&(*j)[key]);
     }
 
+    // If T is of `std::optional<U>`, we allow the entry to be missing,
+    // and when it is present, use the other cases of `get_from_json`
+    // to parse the entry.
     template<typename T>
-    std::optional<T> get_from_json_opt(json *j, const char* key) {
+    typename std::enable_if<IsOptional<T>::value, T>::type
+    get_from_json(json *j, const char* key) {
         if (j->contains(key)) {
-            return get_from_json<T>(j, key);
+            return get_from_json<typename IsOptional<T>::inner_type>(j, key);
         } else {
             return {};
         }
     }
 
-    struct Chat {
-        long id;
-        std::string type;
-        std::optional<std::string> title;
-        std::optional<std::string> username;
+    // Definitions of Telegram data types and auto-generated content
+    TELEGRAM_TYPE_GENERATE(Chat,
+        (long) id,
+        (std::string) type,
+        (std::optional<std::string>) title,
+        (std::optional<std::string>) username,
+    )
 
-        static Chat from_json(json *j);
-    };
+    TELEGRAM_TYPE_GENERATE(User,
+        (long) id,
+        (bool) is_bot,
+        (std::string) username,
+        (std::optional<std::string>) first_name,
+        (std::optional<std::string>) last_name,
+    )
 
-    struct User {
-        long id;
-        bool is_bot;
-        std::string username;
-        std::optional<std::string> first_name;
-        std::optional<std::string> last_name;
+    TELEGRAM_TYPE_GENERATE(Message,
+        (long) message_id,
+        (std::optional<std::string>) text,
+        (Chat) chat,
+        (std::optional<User>) from, // Only present when a message is from real human
+    )
 
-        static User from_json(json *j);
-    };
-
-    struct Message {
-        long message_id;
-        std::optional<std::string> text;
-        Chat chat;
-        std::optional<User> from; // The USER the message is from, None if not from user
-
-        static Message from_json(json *j);
-    };
-
-    struct Update {
-        unsigned long update_id;
-        std::optional<Message> message;
-
-        static Update from_json(json *j);
-    };
+    TELEGRAM_TYPE_GENERATE(Update,
+        (unsigned long) update_id,
+        (std::optional<Message>) message,
+    )
 
     // Generic type of a vector of all the above types
     template<typename T>
--- util.hpp
+++ util.hpp
@@ -5,6 +5,84 @@
 #include <iostream>
 #include <cstdio>
 
+// From SOF
+// <https://stackoverflow.com/questions/319328/how-to-write-a-while-loop-with-the-c-preprocessor>
+// While loop (actually, a `fold` loop) implementation with macros
+#define EMPTY()
+#define DEFER(id) id EMPTY()
+#define OBSTRUCT(id) id DEFER(EMPTY)()
+#define EXPAND(...) __VA_ARGS__
+
+#define EVAL(...)  EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
+#define EVAL1(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
+#define EVAL2(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
+#define EVAL3(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
+#define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
+#define EVAL5(...) __VA_ARGS__
+
+#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
+#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+
+#define CHECK_N(x, n, ...) n
+#define CHECK(...) CHECK_N(__VA_ARGS__, 0,)
+
+#define NOT(x) CHECK(PRIMITIVE_CAT(NOT_, x))
+#define NOT_0 ~, 1,
+
+#define COMPL(b) PRIMITIVE_CAT(COMPL_, b)
+#define COMPL_0 1
+#define COMPL_1 0
+
+#define BOOL(x) COMPL(NOT(x))
+
+#define IIF(c) PRIMITIVE_CAT(IIF_, c)
+#define IIF_0(t, ...) __VA_ARGS__
+#define IIF_1(t, ...) t
+
+#define IF(c) IIF(BOOL(c))
+
+#define WHILE(pred, op, ...) \
+    IF(pred(__VA_ARGS__)) \
+    ( \
+        OBSTRUCT(WHILE_INDIRECT) () \
+        ( \
+            pred, op, op(__VA_ARGS__) \
+        ), \
+        __VA_ARGS__ \
+    )
+#define WHILE_INDIRECT() WHILE
+
+#define NARGS_SEQ(_1,_2,_3,_4,_5,_6,_7,_8,N,...) N
+#define NARGS(...) NARGS_SEQ(__VA_ARGS__, 8, 7, 6, 5, 4, 3, 2, 1)
+
+#define IS_1(x) CHECK(PRIMITIVE_CAT(IS_1_, x))
+#define IS_1_1 ~, 1,
+
+// Predicate: fold until there is only one argument left
+// TODO: extend the above macros so that we can support
+//      more than 8 arguments.
+#define PRED(x, ...) COMPL(IS_1(NARGS(__VA_ARGS__)))
+
+#define M(...) CAT(__VA_ARGS__)
+
+#define REM(...) __VA_ARGS__
+#define EAT(...)
+
+// From <http://pfultz2.com/blog/2012/07/31/reflection-in-under-100-lines/>
+// Though we are not actually using "reflection", I took some idea
+// in my implementation of the auto-generated Telegram types
+// Retrieve the type
+#define TYPEOF(x) DETAIL_TYPEOF(DETAIL_TYPEOF_PROBE x,)
+#define DETAIL_TYPEOF(...) DETAIL_TYPEOF_HEAD(__VA_ARGS__)
+#define DETAIL_TYPEOF_HEAD(x, ...) REM x
+#define DETAIL_TYPEOF_PROBE(...) (__VA_ARGS__),
+// Strip off the type
+#define STRIP(x) EAT x
+// Show the type without parenthesis
+#define PAIR(x) REM x
+// Convert identifier to str
+#define STR(s) #s
+
 // From <https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf>
 template<typename ... Args>
 std::string string_format(const std::string& format, Args ... args) {
